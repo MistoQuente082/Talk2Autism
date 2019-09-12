c6a6f36fbe16b1ed6ee404515e780d0673fce90f
julia Dias <juliia.dias00@gmail.com>
2019-09-12 17:51:28 -0300

    Tab Requisições
    Fiz as telas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  //Dados para os cards
+  req: any[] = [
+    {
+      img: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjslcnHkczkAhVzILkGHWOmDvoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.guarulhoshoje.com.br%2F2019%2F04%2F11%2Fguti-promete-concluir-entrega-de-uniformes-escolares-ate-o-final-deste-mes%2F&psig=AOvVaw0fBp9tL4GP69Ngdu4x5ZmB&ust=1568406998636458',
+      tipo: 'Fardamentos'
+    },
+
+    {
+      img: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjWgaqAk8zkAhVKGLkGHZLLDEsQjRx6BAgBEAQ&url=https%3A%2F%2Fmedium.com%2F%40startupdareal%2Findica%25C3%25A7%25C3%25B5es-de-livros-startup-da-real-42f5df423b59&psig=AOvVaw3yxmGVbkNjnL0wjRvCDHJG&ust=1568407391581914',
+      tipo: 'Módulos'
+    },
+
+    {
+      img: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi42pm-k8zkAhUII7kGHYToCSsQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.homify.com.br%2Ffoto%2F490161%2Fescritorio-advocacia-str-sala-reunioes&psig=AOvVaw1x0olfzzRIHz7lOGW8DoYB&ust=1568407520826955',
+      tipo: 'Reuniões'
+    },
+  ]
+  constructor() { }
 
 }
