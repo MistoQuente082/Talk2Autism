17086397ee2e154968931b0a0c4a14c6ac2d52d9
julia Dias <juliia.dias00@gmail.com>
2019-10-02 17:57:51 -0300

    Imagens de requisições
    Imagens do FIgma

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -14,21 +14,21 @@ export class Tab3Page {
     {
       // O comentario abaixo, permite linha mais longa.
       // tslint:disable-next-line: max-line-length
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmRXd1BSlYbnKZHScUDZUsddT6JmKs15ia46AA1-S5k3LnFZ1YpA',
+      img: '/src/assets/uniforme.png',
       tipo: 'Fardamentos',
     },
 
     {
       // O comentario abaixo, permite linha mais longa.
       // tslint:disable-next-line: max-line-length
-      img: 'https://miro.medium.com/max/2350/1*HoT1qwwVIKjHI6Sjzcj7gg.jpeg',
+      img: '/src/assets/modulos.png',
       tipo: 'Módulos',
     },
 
     {
       // O comentario abaixo, permite linha mais longa.
       // tslint:disable-next-line: max-line-length
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTajfHq73hdOyCoX41To_7hJbbCUpr1tg-JqskVY_hkZGJFcsvZ',
+      img: '/src/assets/reunioes.jpg',
       tipo: 'Reuniões',
     },
   ];
